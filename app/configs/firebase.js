a2e52a276a1c81f65649bea4ca3702797d97b21e
Vincent <vincentmuriuki42@gmail.com>
2018-09-02 10:13:51 +0300

    Initial Commit

---
+++ app/configs/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCixk53ZEGgP18bYJATSyciye3apCUnO7s",
+    authDomain: "awesome-31c20.firebaseapp.com",
+    databaseURL: "https://awesome-31c20.firebaseio.com",
+    projectId: "awesome-31c20",
+    storageBucket: "awesome-31c20.appspot.com",
+    messagingSenderId: "82995552561",
+}
